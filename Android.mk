04fcdd50637e7e762e285d4dba150aee067c42a4
Julien Masson <jmasson@baylibre.com>
2024-01-11 18:25:39 +0100

    [WIP] keymint
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- Android.mk
+++ Android.mk
@@ -25,46 +25,8 @@ $(info CFG_TEE_PLUGIN_LOAD_PATH = ${CFG_TEE_PLUGIN_LOAD_PATH})
 # Include optee-client common config and flags                                 #
 ################################################################################
 include $(LOCAL_PATH)/config.mk
-include $(LOCAL_PATH)/android_flags.mk
 
 optee_CFLAGS = $(CFLAGS)
-
-################################################################################
-# Build libteec.so - TEE (Trusted Execution Environment) shared library        #
-################################################################################
-include $(CLEAR_VARS)
-LOCAL_CFLAGS += $(optee_CFLAGS)
-
-ifneq ($(CFG_TEE_CLIENT_LOG_FILE),)
-LOCAL_CFLAGS += -DTEEC_LOG_FILE=\"$(CFG_TEE_CLIENT_LOG_FILE)\"
-endif
-
-LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_CLIENT_LOG_LEVEL)
-LOCAL_CFLAGS += -DBINARY_PREFIX=\"TEEC\"
-
-LOCAL_SRC_FILES := libteec/src/tee_client_api.c \
-                   libteec/src/teec_trace.c
-ifeq ($(CFG_TEE_BENCHMARK),y)
-LOCAL_CFLAGS += -DCFG_TEE_BENCHMARK
-LOCAL_SRC_FILES += teec_benchmark.c
-endif
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
-                    $(LOCAL_PATH)/libteec/include \
-
-LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE := libteec
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-
-# Build the 32-bit and 64-bit versions.
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TARGET_ARCH := arm arm64
-
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/public
-
-include $(BUILD_SHARED_LIBRARY)
 
 ################################################################################
 # Build libckteec.so                                                           #
